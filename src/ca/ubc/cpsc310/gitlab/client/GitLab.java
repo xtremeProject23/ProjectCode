38c8421d5e7ac59364ee2fb4b11ba7e75c86aa14
Steven <code.workshop@outlook.com>
2014-10-02 17:01:21 -0700

    Changed text in popup window

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -45,6 +45,7 @@ public class GitLab implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 	
-		Window.alert("Loaded");
+		Window.alert("This is my second GitHub change!");
+		Window.alert("This is my second.5 GitHub change!");		
 	}
 }
