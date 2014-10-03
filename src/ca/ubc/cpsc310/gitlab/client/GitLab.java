d1cb21e20b00a5fcb4f9d2dcb0ced5ce58c7556c
Steven <code.workshop@outlook.com>
2014-10-02 20:26:46 -0700

    Comment out

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -58,7 +58,7 @@ public class GitLab implements EntryPoint {
 
 			@Override
 			public void onSuccess(List<IUser> result) {
-				Window.alert("Got list back with " +  result.size() + " entries");
+				// Window.alert("Got list back with " +  result.size() + " entries");
 				
 			}});
 	
